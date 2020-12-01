fab3629ecd932da310e3f04fedc9ca5d0fdba5c6
artinan <61893231+ArtiN4N@users.noreply.github.com>
2020-12-01 13:45:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-var d = new Date();
-console.log(d);
+let dates = {
+  "Mon": "Monday",
+  "Tue": "Tuesday",
+  "Wed": "Wednesday",
+  "Thu": "Thursday",
+  "Fri": "Friday",
+  "Sat": "Saturday",
+  "Sun": "Sunday",
+};
+
+let months = {
+  "Jan": "January",
+  "Feb": "Febuary",
+  "Mar": "March",
+  "Apr": "April",
+  "May": "May",
+  "Jun": "June",
+  "Jul": "July",
+  "Aug": "August",
+  "Sep": "September",
+  "Oct": "October",
+  "Nov": "November",
+  "Dec": "December",
+};
+
+var fullDate = new Date().toString();
+var weekDay = dates[fullDate.substr(0, 3)];
+var monthDay = months[fullDate.substr(4, 3)];
+var numberDay = fullDate.substr(8, 2);
+var yearDay = fullDate.substr(11, 4);
+var time = fullDate.substr(16, 8);
+console.log(weekDay, monthDay, numberDay, yearDay, time);
