87f933f90fef82a78b77182c70bf57a3034ba33b
artinan <61893231+ArtiN4N@users.noreply.github.com>
2020-12-04 18:22:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function updateTime() {
 
 var id = 0
 function addTodo() {
-  let todo = "<input id='tB" + id + "' class='todoBoxes' type='text' placeholder='Write Here...'><button id='tBB" + id + "' class='todoBoxButton' type='button' onclick='addTodo'></button>";
+  let todo = "<input id='tB" + id + "' class='todoBoxes' type='text' placeholder='Write Here...' spellcheck='false'/><button id='tBB" + id + "' class='todoBoxButton' type='button' onclick='addTodo'></button>";
   id++;
   $('#todo').append(todo);
 }
