f333d7cabf4c31ea82b9125bf7f4bfec40744f26
artinan <61893231+ArtiN4N@users.noreply.github.com>
2021-02-22 22:14:37 -0500

    Update chessBoard.js

--- chess/chessBoard.js
+++ chess/chessBoard.js
@@ -1,40 +1,34 @@
+var size = 800;
+
 function startChess() {
   chessBoard.start();
-  chessBoard.create();
 }
 
 var chessBoard = {
   canvas: document.createElement("canvas"),
   start: function() {
-    this.canvas.width = 800;
-    this.canvas.height = 800;
+    this.canvas.width = size;
+    this.canvas.height = size;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-  },
-  create: function() {
     for (i = 0; i < 8; i++) {
       for (o = 0; o < 8; o++) {
-        let white = (i+o % 2 == 0);
-        t = new tile(100, 100, white, o, i);
-        t.draw();
+        if ((i+o) % 2 == 0) {
+          t = new tile(o, i);
+          t.draw();
+        }
       }
     }
   }
 }
 
-function tile(w, h, white, x, y) {
-  this.width = w;
-  this.height = h;
+function tile(x, y) {
   this.x = x;
   this.y = y;
-  this.c = 100;
-  this.color = "rgb(0, 0, 0)";
-  if (white) {
-    this.color = "rgb(255, 255, 255)"
-  }
+  this.c = (size/8);
   this.draw = function() {
     let newTile = chessBoard.context;
-    newTile.fillStyle = this.color;
-    newTile.fillRect(this.x * this.c, this.y * this.c, this.width, this.height);
+    newTile.fillStyle = "rgb(0, 0, 0)";
+    newTile.fillRect(this.x * this.c, this.y * this.c, (size/8), (size/8));
   }
 }
