16a5558b96d040edec64aff0977510063c5b3daf
artinan <61893231+ArtiN4N@users.noreply.github.com>
2021-03-22 17:19:58 -0400

    Update index.js

--- gDevClub/index.js
+++ gDevClub/index.js
@@ -93,40 +93,4 @@ function updateCoin() {
     }
 }
 
-window.addEventListener("keydown", function(event) {
-    switch(event.code) {
-        case "KeyS":
-        case "ArrowDown":
-            yVel = 10.0;
-            break;
-        case "KeyW":
-        case "ArrowUp":
-            yVel = -10.0;
-            break;
-        case "KeyA":
-        case "ArrowLeft":
-            xVel = -10.0;
-            break;
-        case "KeyD":
-        case "ArrowRight":
-            xVel = 10.0;
-            break;
-    }
-}, true);
 
-window.addEventListener("keyup", function(event) {
-    switch(event.code) {
-        case "KeyS":
-        case "ArrowDown":
-        case "KeyW":
-        case "ArrowUp":
-            yVel = 0.0;
-            break;
-        case "KeyA":
-        case "ArrowLeft":
-        case "KeyD":
-        case "ArrowRight":
-            xVel = 0.0;
-            break;
-    }
-}, true);
