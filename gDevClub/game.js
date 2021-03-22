a41d07697f226b30f8d0316c56741725791fe151
artinan <61893231+ArtiN4N@users.noreply.github.com>
2021-03-22 17:36:49 -0400

    Update game.js

--- gDevClub/game.js
+++ gDevClub/game.js
@@ -1,3 +1,7 @@
+let jumpKey = false;
+let leftkey = false;
+let rightKey = false;
+
 let x = 50.0;
 let y = 50.0;
 
@@ -93,3 +97,37 @@ function updateCoin() {
         updateCoin();
     }
 }
+
+window.addEventListener("keydown", function(event) {
+    switch(event.code) {
+        case "KeyW":
+        case "ArrowUp":
+            jumpKey = true;
+            break;
+        case "KeyA":
+        case "ArrowLeft":
+            leftkey = true;
+            break;
+        case "KeyD":
+        case "ArrowRight":
+            rightKey = true;
+            break;
+    }
+}, true);
+
+window.addEventListener("keyup", function(event) {
+    switch(event.code) {
+        case "KeyW":
+        case "ArrowUp":
+            jumpkey = false;
+            break;
+        case "KeyA":
+        case "ArrowLeft":
+            leftKey = false;
+            break;
+        case "KeyD":
+        case "ArrowRight":
+            rightKey = false;
+            break;
+    }
+}, true);
