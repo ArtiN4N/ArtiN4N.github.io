7670873d2c8f47967e41a3bb1caddef1d1552eb1
artinan <61893231+ArtiN4N@users.noreply.github.com>
2021-05-10 13:31:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -137,3 +137,16 @@ function todoPlaceholders(obj) {
   }
   $('#tRB' + obj.id).css("left", len + "px");
 }
+
+const f = document.getElementById('form');
+const q = document.getElementById('query');
+const google = 'https://www.google.com/search?q=';
+
+function submitted(event) {
+  event.preventDefault();
+  const url = google + q.value;
+  const win = window.open(url, '_blank');
+  win.focus();
+}
+
+f.addEventListener('submit', submitted);
